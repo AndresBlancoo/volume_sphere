fd73c3de507e2a63d35dc86e22f31211818bf16b
AndresBlancoo <70651632+AndresBlancoo@users.noreply.github.com>
2020-09-09 10:52:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    Volume_Sphere = 4 / (3 * Pi) * Radio ** 3
+    basic.showNumber(Volume_Sphere)
+})
+let Volume_Sphere = 0
+let Pi = 0
+let Radio = 0
+Radio = 5
+Pi = 3.1416
+basic.forever(function () {
+	
+})
